caafb0be4fc7061a726973079a34b85638affd32
Jason <jbaehne@drivewealth.com>
2023-12-17 14:55:27 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,9 +32,9 @@ module "blog_sg" {
   version             = "5.1.0"
   name                = "blog"
   vpc_id              = data.aws_vpc.default.id
-  ingress_rules       = ["http-80-tcp","https-442-tcp"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
